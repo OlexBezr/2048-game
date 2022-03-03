c7118eae74bac0e929868cdf0123f62f46aef515
OlexBezr <deman26666@gmail.com>
2022-03-03 23:22:42 +0200

    v2

--- model.py
+++ model.py
@@ -1,37 +1,91 @@
-class _2048_model: 
-	@classmethod
-	def print_score(cls, game_field) :
-		row_l = []
-		print('-----')
-		for row in game_field:
-			score_row = []
-			for block in row: 
-				score_row.append( block['score'])
-			print(score_row)
-		print('-----')
-	
-		return row_l
+class _2048_model(): 
+	FIELD_PADDING_TOP = 100
+	FIELD_PADDING_LEFT = 20
+	BLOCK_MARGIN = 10
+
+	def __init__(self) :
+		self.field_padding_left = 20
+		self.field_padding_top = 100
+	@classmethod		
+	def field_render(cls, column, row): 
+		# return like this: 
+		# 
+		# [
+		# 	[
+		# 		{'index': 0, 'score': 0, 'position': {'x': 25, 'y': 105}}, 
+		# 		{'index': 1, 'score': 0, 'position': {'x': 130, 'y': 105}}, 
+		# 		{'index': 2, 'score': 0, 'position': {'x': 235, 'y': 105}}, 
+		# 		{'index': 3, 'score': 0, 'position': {'x': 340, 'y': 105}}
+		# 	], 
+		# 	[
+		# 		{'index': 4, 'score': 0, 'position': {'x': 25, 'y': 210}}, 
+		# 		{'index': 5, 'score': 0, 'position': {'x': 130, 'y': 210}}, 
+		# 		{'index': 6, 'score': 0, 'position': {'x': 235, 'y': 210}}, 
+		# 		{'index': 7, 'score': 0, 'position': {'x': 340, 'y': 210}}
+		# 	]
+		# 	....
+		# ]
+
+		game_field = []
+		index_ = 0
 		
+		block_widht, block_height = 105, 105
+		
+		for i in range(0, row):
+			game_field.append([])
+			if i == 0 : 
+				position_y = _2048_model.FIELD_PADDING_TOP + _2048_model.BLOCK_MARGIN*i
+			else :
+				position_y = block_height * i + _2048_model.BLOCK_MARGIN*i + _2048_model.FIELD_PADDING_TOP 
+			
+			for j in range(0, column):
+				if j == 0: 
+					position_x = _2048_model.FIELD_PADDING_LEFT + _2048_model.BLOCK_MARGIN*j
+				else : 
+					position_x = block_widht * j + _2048_model.BLOCK_MARGIN*j + _2048_model.FIELD_PADDING_LEFT 
+					
+				game_field[-1].append({
+					'index' : index_,
+					'score' : 0,
+					'position' : {
+						'x': position_x,
+						'y': position_y
+					},
+					'width' : block_widht,
+					'height' : block_height,
+
+					'step_top' : 0,
+					'step_down' : 0,
+					'step_right' : 0,
+					'step_left' : 0
+				})
+
+				index_ +=1
+		return game_field
+
+
 	@classmethod
 	def move_top(cls, game_field) :
 		def step_top(): 
+			some_move = False
 			iteration = 1
-
 			while iteration < len(game_field): # прохожу column N раз что бы сдвинуть все
 				for row in game_field: 
-					print('+++',iteration)
-					cls.print_score(game_field)
 					index_r = game_field.index(row)
 					if not index_r-1 < 0: 
 						for block in row: 
 							index_b = row.index(block)
 							try: 
-								if game_field[index_r-1][index_b]['score'] == 0: # если выше чисто
+								if game_field[index_r-1][index_b]['score'] == 0 and block['score'] != 0: # если выше чисто и текущий блок не 0
 									game_field[index_r-1][index_b]['score'] = block['score'] # заменяю score
 									block['score'] = 0 # текущий блок очищаю
+
+									game_field[index_r-1][index_b]['step_top'] += 1
+									some_move = True
 							except IndexError: 
-								continue
+								pass
 				iteration +=1
+			return some_move
 		def join_blocks() :
 			for row in game_field: 
 				index_r = game_field.index(row)
@@ -44,38 +98,39 @@ class _2048_model:
 								game_field[index_r+1][index_b]['score'] = 0 # тот что ниже очищаю
 								step_top() # еще раз поднимаю все блоки
 						except IndexError: 
-							continue
+							pass
 
-		step_top()
+		some_move = step_top()
 		join_blocks()
+		return some_move
 
 	@classmethod
 	def move_down(cls, game_field) :
 		def step_down(): 
+			some_move = False
 			iteration = 1
 			while iteration < len(game_field): # прохожу column N раз что бы сдвинуть все
 				for row in game_field: 
-					print('+++',iteration)
-					cls.print_score(game_field)
 					index_r = game_field.index(row)
 					if not index_r+1 > len(game_field): 
 						for block in row: 
 							index_b = row.index(block)
-							
 							try: 
 								if game_field[index_r+1][index_b]['score'] == 0: # если выше чисто
 									game_field[index_r+1][index_b]['score'] = block['score'] # заменяю score
 									block['score'] = 0 # текущий блок очищаю
+									some_move = True
 							except IndexError: 
-								continue
+								pass
 				iteration +=1
+			return some_move
 		def join_blocks() :
 			# здесь нужно проходить row с конца
-			len_game_field = len(game_field)-1
+			count_row = len(game_field)-1
 
-			while len_game_field > 0 : 
+			while count_row >= 0 : 
 			# for row in game_field: 
-				row = game_field[len_game_field]
+				row = game_field[count_row]
 				index_r = game_field.index(row)
 				if not index_r-1 < 0: 
 					for block in row: 
@@ -86,51 +141,91 @@ class _2048_model:
 								game_field[index_r-1][index_b]['score'] = 0 # тот что ниже очищаю
 								step_down() # еще раз поднимаю все блоки
 						except IndexError: 
-							continue
-				len_game_field -=1
+							pass
+				count_row -=1
 
-		step_down()
+		some_move = step_down()
 		join_blocks()
+		return some_move
 		
-	# [
-	# 	[0,2,,0]
-	# 	[0,4,0,0]
-	# 	[0,2,0,0]
-	# 	[0,0,0,0]
-	# ]
 	@classmethod
 	def move_left(cls, game_field) :
-		iteration = 1
-		while iteration < len(game_field): # прохожу row N раз что бы сдвинуть все
+		def step_left():
+			some_move = False
+			iteration = 1
+			while iteration < len(game_field): # прохожу row N раз что бы сдвинуть все
+				for row in game_field: 
+					for block in row:
+						index_b = row.index(block)
+						if not index_b-1 < 0: 
+							try:
+								if row[index_b-1]['score'] == 0: # если левый блок пустой
+									row[index_b-1]['score'] = block['score'] # заменяю score
+									block['score'] = 0 # текущий блок очищаю
+									some_move = True
+							except IndexError: 
+								pass
+				iteration +=1
+			return some_move
+		def join_blocks():
 			for row in game_field: 
-				print('+++',iteration)
-				cls.print_score(game_field)
-				for block in row:
+				# index_r = game_field.index(row)
+				for block in row: 
 					index_b = row.index(block)
-					if not index_b-1 < 0: 
-						try:
-							if row[index_b-1]['score'] == 0: # если левый блок пустой
-								row[index_b-1]['score'] = block['score'] # заменяю score
-								block['score'] = 0 # текущий блок очищаю
+					if not index_b-1 < 0:
+						try: 
+							if row[index_b-1]['score'] == row[index_b]['score']: # если левый блок такой же как и текущий
+								row[index_b]['score'] = row[index_b]['score']*2 # удваитаю текущий блок
+								row[index_b-1]['score'] = 0 # тот что левее очищаю
+								step_left() # еще раз сдвигаю все блоки
 						except IndexError: 
-							continue
-			iteration +=1
+							pass
+		some_move = step_left()
+		join_blocks()
+		return some_move
 	
 	@classmethod
 	def move_right(cls, game_field) :
-		iteration = 1
-		while iteration < len(game_field): # прохожу row N раз что бы сдвинуть все
+		def step_right():
+			some_move = False
+			iteration = 1
+			while iteration < len(game_field): # прохожу row N раз что бы сдвинуть все
+				for row in game_field: 
+					for block in row:
+						index_b = row.index(block)
+						if not index_b+1 > len(row): 
+							try: 
+								if row[index_b+1]['score'] == 0: # если левый блок пустой
+									row[index_b+1]['score'] = block['score'] # заменяю score
+									row[index_b]['score'] = 0 # текущий блок очищаю
+									some_move = True
+							except IndexError: 
+								pass
+				iteration +=1
+			return some_move
+
+		def join_blocks():
+			# здесь так же нужно начиноть с другой сторовы, иначе все что в row есть соединится
+			# like [2,2,2,2] -> [0,0,0,8], а должно быть [0,0,4,4]
 			for row in game_field: 
-				for block in row:
+				# index_r = game_field.index(row)a
+				count_block = len(row)-1
+				while count_block >= 0:
+				# for block in row: 
+					block = row[count_block]
 					index_b = row.index(block)
-					if not index_b+1 > len(row): 
+					if not index_b+1 > len(row):
 						try: 
-							if row[index_b+1]['score'] == 0: # если левый блок пустой
-								row[index_b+1]['score'] = block['score'] # заменяю score
-								row[index_b]['score'] = 0 # текущий блок очищаю
+							if row[index_b+1]['score'] == row[index_b]['score']: # если правый блок такой же как и текущий
+								row[index_b]['score'] = row[index_b]['score']*2 # удваитаю текущий блок
+								row[index_b+1]['score'] = 0 # тот что правее очищаю
+								step_right() # еще раз сдвигаю все блоки
 						except IndexError: 
-							continue
-			iteration +=1
+							pass
+					count_block -= 1
+		some_move = step_right()
+		join_blocks()
+		return some_move
 
 	@classmethod
 	def get_empty_blocks(cls, game_field) :
@@ -161,3 +256,59 @@ class _2048_model:
 		else : 
 			game_field[ row ][ col ]['score'] = 2
 		return random_pos
+
+	@classmethod
+	def get_total_score(cls, game_field):
+		total_score = 0
+		for row in game_field:
+			for block in row:
+				total_score += block['score']
+
+		return str(total_score)
+
+	@classmethod
+	def game_over(cls, game_field) :
+		g = []
+		for row in game_field:
+			for block in row: 
+				g.append( block['score'] )
+
+		if all(g): # если все блоки заполнены
+			available_move = []
+			for row in game_field:
+				index_r = game_field.index(row)
+				for block in row: 
+					index_b = row.index(block)
+					try: 
+						if game_field[index_r-1][index_b]['score'] == block['score']: # верхний
+							available_move = True
+							break
+					except: 
+						pass
+
+					try: 
+						if game_field[index_r+1][index_b]['score'] == block['score'] : # нижний
+							available_move = True
+							break
+					except: 
+						pass
+
+					try:
+						if game_field[index_r][index_b+1]['score'] == block['score']: # правый
+							available_move = True
+							break
+					except: 
+						pass
+
+					try: 
+						if game_field[index_r][index_b-1]['score'] == block['score'] : # левый
+							available_move = True
+							break
+					except: 
+						pass
+					
+			return available_move
+
+
+		return False
+
--- view.py
+++ view.py
@@ -1,71 +1,37 @@
-class _2048_view:
-	def __init__(self) :
-		self.fps = 30
-		self.colors = {
-			'black' : (0,0,0),
-			'white' : (255,255,255)
-		}
+from model import _2048_model as model_
 
-	@classmethod		
-	def field_render(cls, column, row): 
-		# return like this: 
-		# 
-		# [
-		# 	[
-		# 		{'index': 0, 'score': 0, 'position': {'x': 25, 'y': 105}}, 
-		# 		{'index': 1, 'score': 0, 'position': {'x': 130, 'y': 105}}, 
-		# 		{'index': 2, 'score': 0, 'position': {'x': 235, 'y': 105}}, 
-		# 		{'index': 3, 'score': 0, 'position': {'x': 340, 'y': 105}}
-		# 	], 
-		# 	[
-		# 		{'index': 4, 'score': 0, 'position': {'x': 25, 'y': 210}}, 
-		# 		{'index': 5, 'score': 0, 'position': {'x': 130, 'y': 210}}, 
-		# 		{'index': 6, 'score': 0, 'position': {'x': 235, 'y': 210}}, 
-		# 		{'index': 7, 'score': 0, 'position': {'x': 340, 'y': 210}}
-		# 	]
-		# 	....
-		# ]
+class _2048_view():
 
-
-		game_field = []
-		index_ = 0
-		
-		field_padding_left = 20
-		field_padding_top = 100
-		
-		block_margin = 10
-		block_widht, block_height = 105, 105
-		
-		for i in range(0, row):
-			game_field.append([])
-			if i == 0 : 
-				position_y = field_padding_top + block_margin*i
-			else :
-				position_y = block_height * i + block_margin*i + field_padding_top 
-			
-			for j in range(0, column):
-				if j == 0: 
-					position_x = field_padding_left + block_margin*j
-				else : 
-					position_x = block_widht * j + block_margin*j + field_padding_left 
-					
-				game_field[-1].append({
-					'index' : index_,
-					'score' : 0,
-					'position' : {
-						'x': position_x,
-						'y': position_y
-					},
-					'width' : block_widht,
-					'height' : block_height
-				})
-
-				index_ +=1
-		return game_field
 
 	@classmethod
-	def draw_field(cls, pg, screen, game_field):
-		# print(cls.colors)
+	def print_in_cmd(cls, game_field) :
+		row_l = []
+		print('-----')
+		for row in game_field:
+			score_row = []
+			for block in row: 
+				score_row.append( block['score'])
+			print(score_row)
+		print('-----')
+	
+		return row_l
+
+
+	# @classmethod
+	# def draw_field(cls, pg, screen, game_field):
+	# 	# screen.fill( (0,0,0) )
+	# 	for row in game_field: 
+	# 		for block in row: 
+
+	# 			cls.draw_block( pg, screen, block)
+	# 			cls.draw_total_score( pg, screen, game_field)
+	# 			cls.draw_block_score( pg, screen, block)
+
+	# 			pg.display.flip()
+	
+	@classmethod
+	def draw_block(cls, pg, screen, block):
+		# нужно сделать генератор цветов
 		BLOCK_COLOR = {
 			'0': (214,205,196),
 			'2' : (239,230,221),
@@ -80,9 +46,82 @@ class _2048_view:
 			'1024':(239,198,58),
 			'2048': (239,194,41)
 		}
-		for row in game_field: 
-			for block in row: 
-				pg.draw.rect( screen, BLOCK_COLOR[ str(block['score']) ], ( block['position']['x'], block['position']['y'], block['width'], block['height']))
-				pg.draw.rect( screen, (191,173,163), ( block['position']['x'], block['position']['y'], block['width'], block['height']), 10)
-				# screen.blit(ball, ballrect)
-				pg.display.flip()
+		#block
+		block['rect'] = pg.draw.rect( screen, BLOCK_COLOR[ str(block['score']) ], ( block['position']['x'], block['position']['y'], block['width'], block['height']))
+		cls.draw_block_score(pg, screen, block)
+		pg.display.update()
+		pg.display.flip()
+
+	@classmethod
+	def draw_block_score(cls, pg, screen, block) :
+		if block['score'] != 0 : 
+			font = pg.font.Font(None, 40)
+			text = font.render( str( block['score'] ) , True, ( 180, 0, 0) )
+
+			text_pos_center_x = block['position']['x'] + (block['width'] / 2 - text.get_rect().width / 2)
+			text_pos_center_y = block['position']['y'] + (block['height'] / 2 - text.get_rect().height / 2)
+
+			screen.blit( text, ( text_pos_center_x, text_pos_center_y ) )
+
+	@classmethod
+	def draw_total_score(cls, pg, screen, game_field) : 
+		total_score = model_.get_total_score(game_field)
+
+		font = pg.font.Font(None, 30)
+		text = font.render( 'Score: ' + total_score, True, ( 180, 0, 0) )
+
+		screen.blit( text, ( 40, 10 ) )
+		pg.display.update()
+		pg.display.flip()
+
+	# @classmethod
+	# def animation_move_block_top(cls, game_field):
+	# 	# анимация должно быть 0.5 секунд
+	# 	# колонки двигаются с разно скоростью что бы они останавливали анимацию в одно время 
+	# 	# при соединении нужно сделать перемищение нижнего блока на верхний. и заменить эго на новый блок 
+	# 	# [
+	# 	# 	[0,0,2,0]    [2,4,2,2]
+	# 	# 	[0,2,0,2]    [0,0,4,0]
+	# 	# 	[0,2,4,0]    [0,0,0,0]
+	# 	# 	[2,0,0,0]    [0,0,0,0]
+	# 	# ]
+
+	# 		# for new_row in new_game_field : 
+
+
+	# 	# for old_row in old_game_field :
+	# 	# 	new_row = new_game_field[ old_game_field.index(old_row) ]
+
+	# 	# 	for index_b in range(0, len(old_game_field)-1)
+	# 	# 		if old_row[]
+	# 	for row in game_field : 
+	# 		for block in row : 
+	# 			if block["step_top"] != 0 : 
+
+
+	# @classmethod
+	# def animation_move_block_top(cls, block):
+
+	# 	# просто переместить плавно вверх блок
+
+	# 	#
+
+	# 	step_one_tick = (block['height']+ model_.BLOCK_MARGIN) / 100
+
+	# 	need_be_on = block['positon']['y']- model_.BLOCK_MARGIN - block['height']
+
+
+	# 	block['positon']['x']
+	# 	block['positon']['y']
+
+	# @classmethod
+	# def animation_move_block_top(cls, block):
+	# 	if block["step_top"] != 0 : 
+	# 		step_one_tick = (( block['height'] + model_.BLOCK_MARGIN) / 100) * block["step_top"]
+	# 		block['position']['y'] -= step_one_tick
+	# 		block["step_top"] -= 1
+
+
+
+
+
